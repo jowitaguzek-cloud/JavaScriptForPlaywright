3dcfa481d8f32196e968a191e3d2838d6afa3190
Jowita Guzek <jowita.guzek@radpoint.pl>
2026-02-25 08:44:15 +0100

    Uzupełnienie script.js

--- domManipulation/script.js
+++ domManipulation/script.js
@@ -1,7 +1,20 @@
-
-
 // 1. Dodanie nowego elementu przy ładowaniu strony
+const newElement = document.createElement('div');
+newElement.textContent = "Przycisk";
+document.body.appendChild(newElement);
+
 // 2. Zmiana koloru paragrafu
+const pierwszyParagraf = document.querySelector('p');
+pierwszyParagraf.style.color = 'green';
+
 // 3. Dla chętnych: pobierz element paragrafu nr 2.
+const drugiParagraf = document.querySelectorAll('p')[1];
+
 // Pobierz element przycisku i uruchom na nim metodę addEventListener na zdarzenie ‚click’.
+const button = document.querySelector('button');
+
 // W metodzie addEventListener zmień kolor przycisku.
+button.addEventListener('click', function() {
+  this.style.backgroundColor = 'pink';
+});
+
